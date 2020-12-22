17ca0eb29e6872a77c939273b4f678bed53d7b8b
AsHuOvSo <asta@avra.dk>
2020-12-22 16:28:30 +0100

    Created save function !!NOT TESTED!!

--- src/sample/NewProject.java
+++ src/sample/NewProject.java
@@ -1,9 +1,6 @@
 package sample;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,17 +9,22 @@ import static sample.Menu.ProgramMenu;
 
 public class NewProject {
     static Scanner input = new Scanner(System.in);
-
     public static void CreateProject() {
+        //Eksempel på try/catch
+    try {
         System.out.print("Skriv navnet på opskriften:");
         String name = input.nextLine();
-        /*File newProject = new File(name + ".txt");*/
+        File newProject = new File("C:/Users/AstaOvergaard-Sørens/IdeaProjects/SimpleCookBookDummy/src/Saved/" + name + ".txt");
+        if(newProject.createNewFile())System.out.println("Success!");
+        else System.out.println ("Error, file already exists.");
+
         System.out.print("Tilføj ingredient:");
         List<String> ingredientsRecipe = new ArrayList<>();
         ingredientsRecipe.add(input.nextLine());
         System.out.println("Flere ingredientser?");
         System.out.println("Ja/Nej");
         String yesNo = input.nextLine();
+        //Eksempel på while løkke
         while (yesNo.equalsIgnoreCase("Ja")) {
             System.out.print("Tilføj ingredient:");
             ingredientsRecipe.add(input.nextLine());
@@ -49,17 +51,46 @@ public class NewProject {
         int nextNumber = input.nextInt();
         if (nextNumber == 1) {
             ProgramMenu();
-        }
-        else if (nextNumber == 2) {
+        } else if (nextNumber == 2) {
             System.out.println(name);
             System.out.println("");
             System.out.println(ingredientsRecipe);
             System.out.println("");
             System.out.println(steps);
-        }
-        else {
+
+            System.out.println("Gem opskrift?");
+            System.out.println("Ja/Nej");
+            yesNo = input.nextLine();
+            if (yesNo.equalsIgnoreCase("Ja")) {
+                FileWriter writeToFile = new FileWriter(name + ".txt");
+                writeToFile.write(name);
+                try{
+                    FileOutputStream writeData = new FileOutputStream("peopledata.ser");
+                    ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
+
+                    writeStream.writeObject(ingredientsRecipe);
+                    writeStream.flush();
+                    writeStream.close();
+
+                    writeStream.writeObject(steps);
+                    writeStream.flush();
+                    writeStream.close();
+
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else {
+                ProgramMenu();
+            }
+        } else {
             System.out.println("Noget gik galt");
         }
+    }
+    catch(IOException ioe) {
+        ioe.printStackTrace();
+    }
     }
 
 
--- src/sample/Search.java
+++ src/sample/Search.java
@@ -1,4 +1,22 @@
 package sample;
 
+import java.io.File;
+
 public class Search {
+
+    public static void searchForRecipe() {
+    File dir = new File("C:/Users/AstaOvergaard-Sørens/IdeaProjects/SimpleCookBookDummy/src/Saved");
+    String[] children = dir.list();
+
+      if (children == null) {
+        System.out.println("does not exist or is not a directory");
+    } else {
+        for (int i = 0; i < children.length; i++) {
+            String filename = children[i];
+            System.out.println(filename);
+        }
+    }
+    }
+
 }
+
